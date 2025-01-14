238256ad2acda42cbb773bca8a278204d991fd54
CAS <csamuel@brearley.org>
2025-01-14 16:33:28 -0500

    Removed everything

--- Actor.java
+++
@@ -1,6 +0,0 @@
-interface Actor {
-    void move();      // Define movement behavior
-    char getSymbol(); // Return the symbol representing the actor
-    int getX();       // Get the X-coordinate of the actor
-    int getY();       // Get the Y-coordinate of the actor
-}
